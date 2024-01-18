218cc86a3d750c4e6ef82cb983a4d168bbdf2452
batoul25 <batouljdid25@gmail.com>
2024-01-18 12:52:49 +0300

    add inboxMessages controller (contact us form) <api>

--- app/Http/Controllers/Api/InboxMessagesController.php
+++ app/Http/Controllers/Api/InboxMessagesController.php
@@ -2,63 +2,105 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\InboxMessagesRequest;
+use App\Http\Resources\InboxMessagesresource;
 use App\Models\InboxMessages;
+use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
 
 class InboxMessagesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    use ApiResponse;
+
+    //show all the inbox messages
     public function index()
     {
-        //
+        //get all inbox messages info...
+        $inboxMessages = InboxMessages::with('admin')->get();
+
+        //in this condition we are checking if the inbox_messages table is empty or not.
+         if($inboxMessages->isNotEmpty())
+         {
+             return $this->successResponse(InboxMessagesresource::collection($inboxMessages),'Data fetched successfully',200);
+         }
+
+         return $this->errorResponse('there is no data in the table',401);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    //add a new messages through the contact us form
+    public function store(InboxMessagesRequest $request)
     {
-        //
+        $val_request = $request->validated();
+
+
+            $newMessage= InboxMessages::create([
+                'admin_id' => $val_request['admin_id'],
+                'service' => $val_request['service'] ,
+                'name'    => $val_request['name'],
+                'companyName' => $val_request['companyName'],
+                'number' => $val_request['number'] ,
+                'position' => $val_request['position'],
+                'email' => $val_request['email'],
+                'message' => $val_request['message']
+            ]);
+            if($newMessage)
+            {//check if it's added successfully.
+                return $this->successResponse($newMessage,'Message added successfully',201);
+            }
+            return $this->errorResponse('there was an error adding the message!',401);
+
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\InboxMessages  $inboxMessages
-     * @return \Illuminate\Http\Response
-     */
-    public function show(InboxMessages $inboxMessages)
+    //show a specific existing inbox message
+    public function show($id)
     {
-        //
+        try{
+            $inboxMessage =InboxMessages::find($id);
+            return $this->successResponse(new InboxMessagesresource($inboxMessage), 'Retrived Successfully');
+        }catch(\Exception $exp){
+            return $this->errorResponse('The inbox message is not found',401);
+        }
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\InboxMessages  $inboxMessages
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, InboxMessages $inboxMessages)
+
+    //update a specific message
+    public function update(InboxMessagesRequest $request, $id)
     {
-        //
+        $val_request = $request->validated();//this is the new values, we validate them
+        //find the desired review by id.
+        $inboxMessage = InboxMessages::find($id);
+
+        //in this condition we are checking if the inbox message record exits or not.
+
+        if($inboxMessage)//if it exists
+        {
+            $updated_message = $inboxMessage->update($val_request);
+            return $this->successResponse($updated_message,'Inbox Message updated successfully',200);
+        }
+
+        //if it does not exist it will break the if statement and return the error response
+        return $this->errorResponse('this message does not exists',401);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\InboxMessages  $inboxMessages
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(InboxMessages $inboxMessages)
+   //delete a specific inbox message
+    public function destroy($id)
     {
-        //
+        //find the desired message by id.
+        $inboxMessage = InboxMessages::find($id);
+        //in this condition we are checking if the message record exits or not.
+        if($inboxMessage)
+        {
+            $inboxMessage->delete();
+            return $this->successResponse($inboxMessage,'Message removed successfully',200);
+        }
+        //if it does not exist it will break the if statement and return the error response
+
+        return $this->errorResponse('this message doesnt exists',401);
+    }
+
+    public function destroy_all()
+    {
+        InboxMessages::truncate();
+        return $this->successResponse(null,'all inbox messages removed successfully',200);
     }
 }
